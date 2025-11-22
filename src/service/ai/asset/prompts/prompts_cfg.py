30c1e076f4df74f0ec4ca111366f6f3fb6a94f3f
Ebee1205 <spiverain02@gmail.com>
2025-11-22 15:26:20 +0900

    feat: 프롬프트 수정

--- src/service/ai/asset/prompts/prompts_cfg.py
+++ src/service/ai/asset/prompts/prompts_cfg.py
@@ -3,5 +3,5 @@ import service.ai.asset.prompts.doq_prompts as doq_prompts
 # 시스템 프롬프트 
 SYSTEM_PROMPTS = [
     doq_prompts.INITIAL_PROMPT,   
-    # bantori_prompts.ANALYSIS_CRITERIA,   
+    doq_prompts.USER_CONTEXT_PROMPT,   
 ]
