dfae5c310a37bfb0caa9cf9dbf182dd205ddcc11
Ebee1205 <spiverain02@gmail.com>
2025-12-10 13:57:19 +0900

    feat: api 키 설정

--- src/service/ai/llm_manager.py
+++ src/service/ai/llm_manager.py
@@ -22,9 +22,9 @@ class LLMManager:
 
         if self.provider == "gemini":
             api_key = GEMINI_API_KEY
-            # api_key = os.environ.get("GEMINI_API_KEY")
-            if not api_key:
-                raise ValueError("GEMINI_API_KEY 환경 변수가 설정되지 않았습니다.")
+            api_key = os.environ.get("GEMINI_API_KEY")
+            # if not api_key:
+            #     raise ValueError("GEMINI_API_KEY 환경 변수가 설정되지 않았습니다.")
             
             genai.configure(api_key=api_key)
             self.gemini_model = genai.GenerativeModel(self.model)
